ec649d2815624be930022bedf4dde70928dde651
Nigel Martinez <nigel-martinez@hotmail.com>
2015-02-10 11:39:25 -0500

    Added Rock Spawning
    Temporary script code has them despawn after a set time. Will have them
    destroyable by colliding with players or being hit by player attacks
    later.

---
+++ RockPaperScissors/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public float spawnTime = 5f; // The amount of time between each spawn.
+	public float spawnDelay = 3f; // The amount of time before spawning starts.
+	public bool onLeft = true; // The location of the spawner in relation to the map.
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", spawnDelay, spawnTime); // Call the Spawn function between spawn times.
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn () {
+		GameObject newRock = Resources.Load("Rock") as GameObject;
+
+		if (onLeft) {
+			onLeft = false;
+			Instantiate (newRock, transform.position, transform.rotation); //Spawn a rock on the left.
+		}
+		else {
+			Vector3 spawnLocation = transform.position;
+			spawnLocation.x *= -1;
+
+			onLeft = true;
+			Instantiate (newRock, spawnLocation, transform.rotation); //Spawn a rock on the left.
+		}
+	}
+}
---
+++ RockPaperScissors/Assets/Scripts/rockBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockBehaviour : MonoBehaviour {
+	public float lifeTime = 8f; //TEMP: Amount of time before this object is destroyed.
+	//TODO: Have rock destroyed when it hits either players or other rocks.
+
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Destroy", lifeTime); //TEMP: Destroy this object after some time.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Destroy () {
+		Destroy (this.gameObject);
+	}
+}
