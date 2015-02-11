906c91d2c8b8904c2faf27ff25e7f5b811a99763
DaedalusLost <gibsonb@mail.uoguelph.ca>
2015-02-10 22:23:35 -0500

    Health label fix
    Increased the size of the health labels using a GUIStyle.

--- RockPaperScissors/Assets/Scripts/healthDisplay.cs
+++ RockPaperScissors/Assets/Scripts/healthDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class healthDisplay : MonoBehaviour {
 	Player p1, p2;
+	GUIStyle labelFontSize;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,10 @@ public class healthDisplay : MonoBehaviour {
 		p1 = (Player)tmpGameObject.GetComponent(typeof(Player));
 		tmpGameObject = GameObject.Find ("Player2");
 		p2 = (Player)tmpGameObject.GetComponent(typeof(Player));
+		
+		labelFontSize = new GUIStyle();
+		labelFontSize.fontSize = 30;
+		labelFontSize.normal.textColor = Color.white;
 	}
 
 	// Update is called once per frame
@@ -19,7 +24,7 @@ public class healthDisplay : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUI.Label (new Rect(10, 10, 300, 300), p1.playerHealth.health.ToString());
-		GUI.Label (new Rect(1200, 10, 300, 300), p2.playerHealth.health.ToString());
+		GUI.Label (new Rect(10, 10, 300, 300), p1.playerHealth.health.ToString(), labelFontSize);
+		GUI.Label (new Rect(1200, 10, 300, 300), p2.playerHealth.health.ToString(), labelFontSize);
 	}
 }
