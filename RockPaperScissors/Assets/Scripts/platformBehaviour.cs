3302451980bb83c09fe5b146cc2d29eef9985c72
Nigel Martinez <nigel-martinez@hotmail.com>
2015-02-20 14:51:37 -0500

    Implemented One-Way Platforms
    It's currently only one Battlefield, specifically the left sub platform.

---
+++ RockPaperScissors/Assets/Scripts/platformBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformBehaviour : MonoBehaviour {
+	
+	public BoxCollider2D mainCollider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player")
+			Physics2D.IgnoreCollision(mainCollider, other.gameObject.collider2D, true);
+
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			if(other.gameObject.transform.position.y > this.gameObject.transform.position.y)
+				Physics2D.IgnoreCollision (mainCollider, other.gameObject.collider2D, false);
+		}
+	}
+	
+}
