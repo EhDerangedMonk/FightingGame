b127119f823b5cf91110f80d8e7e93a33ff73da2
Nigel Martinez <nigel-martinez@hotmail.com>
2015-03-05 01:24:46 -0500

    Adjusted Hang Triggers
    To keep consistency, they are now trigger colliders.

--- RockPaperScissors/Assets/Scripts/ledgeBehaviour.cs
+++ RockPaperScissors/Assets/Scripts/ledgeBehaviour.cs
@@ -8,27 +8,37 @@ using System.Collections;
 
 public class ledgeBehaviour : MonoBehaviour {
 
-	public Transform hangCheck;
-	public LayerMask players;
+	private bool rightLedge = false;
 	private bool hang = false;
-	private float hangRadius = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-	
+		if (this.name == "leftHangCheck")
+			rightLedge = false;
+		else
+			rightLedge = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (hang)
+			print ("Hang triggered.");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player")
+			hang = true;
+
+	}
 	
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.gameObject.tag == "Player")
+			hang = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.tag == "Player")
+			hang = false;
 	}
 
-	void FixedUpdate() {
-		//hang = Physics2D.OverlapCircle (hangCheck.position, hangRadius, players);
-
-		// if (hang)
-		// 	Debug.Log ("Player should be hanging!");
-		// else
-		// 	Debug.Log ("Player isn't hanging!");
-	}
 }
