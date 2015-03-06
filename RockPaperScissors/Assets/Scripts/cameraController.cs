9cf7e60dcfd1afd0d5a08a23c5c4b754fc43c425
Nigel Martinez <nigel-martinez@hotmail.com>
2015-03-06 15:27:57 -0500

    Okay? GitHub realized there was more?

--- RockPaperScissors/Assets/Scripts/cameraController.cs
+++ RockPaperScissors/Assets/Scripts/cameraController.cs
@@ -11,20 +11,21 @@ public class cameraController : MonoBehaviour {
 		Elements of code dervied from a tutorial by Ryan Nielson.
 		http://nielson.io/2014/03/making-a-target-tracking-orthographic-camera-in-unity/
 	*/
-
+	
 	private float boundingBoxPadding = 2f;
 	private float minimumOrthographicSize = 5f;
 	private float zoomSpeed = 20f;
 	private Camera cam;
-
+	private Transform[] targets;
+	private int targetSize;
+	
 	//Boundaries and default camera location may differ between maps.
-	public Transform[] targets;
 	public GameObject defaultCam;
 	public GameObject topBounds;
 	public GameObject leftBounds;
 	public GameObject rightBounds;
 	public GameObject bottomBounds;
-
+	
 	// Use this for initialization
 	void Start () {
 		cam = GetComponent<Camera>();
@@ -35,14 +36,35 @@ public class cameraController : MonoBehaviour {
 	void Update () {
 		
 	}
-
+	
 	void LateUpdate()
 	{
+		findTargets ();
 		Rect boundingBox = CalculateTargetsBoundingBox();
 		transform.position = CalculateCameraPosition(boundingBox);
 		cam.orthographicSize = CalculateOrthographicSize(boundingBox);
 	}
-
+	
+	void findTargets() {
+		Player subject;
+		GameObject[] players;
+		
+		targets = new Transform[5];
+		targets [0] = defaultCam.transform;
+		targetSize = 0;
+		
+		players = GameObject.FindGameObjectsWithTag ("Player");
+		
+		foreach (GameObject player in players) {
+			subject = (Player)player.gameObject.GetComponent(typeof(Player));
+			
+			if(!subject.playerHealth.isDead()) {
+				targetSize++;
+				targets[targetSize] = player.transform;
+			}
+		}
+	}
+	
 	/*
      * DESCR: Calculates an rectangle that contains the players.
      * PRE: NONE
@@ -54,36 +76,36 @@ public class cameraController : MonoBehaviour {
 		float maxX = Mathf.NegativeInfinity;
 		float minY = Mathf.Infinity;
 		float maxY = Mathf.NegativeInfinity;
-
+		
 		//Find the dimensions of a box that will contain all the targets, a.k.a. players.
 		//DefaultCam is among the targets to avoid any 'accessing empty object' errors.
-		foreach (Transform target in targets) {
-			Vector3 position = target.position;
-
+		for (int i = 0; i <= targetSize; i++) {
+			Vector3 position = targets[i].position;
+			
 			//If the player goes too high, adjust the value to the top boundary.
-			if(target.position.y > topBounds.transform.position.y)
+			if(position.y > topBounds.transform.position.y)
 				maxY = topBounds.transform.position.y;
 			else
 				maxY = Mathf.Max(maxY, position.y);
-
+			
 			//Likewise for a player going too low.
-			if(target.position.y < bottomBounds.transform.position.y)
+			if(position.y < bottomBounds.transform.position.y)
 				minY = bottomBounds.transform.position.y;
 			else
 				minY = Mathf.Min(minY, position.y);
-
+			
 			//Likewise for a player going too far to the left.
-			if(target.position.x < leftBounds.transform.position.x)
+			if(position.x < leftBounds.transform.position.x)
 				minX = leftBounds.transform.position.x;
 			else
 				minX = Mathf.Min(minX, position.x);
-
+			
 			//Liksewise for a player going too far to the right.
-			if(target.position.x > rightBounds.transform.position.x)
+			if(position.x > rightBounds.transform.position.x)
 				maxX = rightBounds.transform.position.x;
 			else
 				maxX = Mathf.Max(maxX, position.x);
-
+			
 			/* Nielson's orginal calculations, which did not account for boundaries.
 			minX = Mathf.Min(minX, position.x);
 			minY = Mathf.Min(minY, position.y);
@@ -94,7 +116,7 @@ public class cameraController : MonoBehaviour {
 		
 		return Rect.MinMaxRect(minX - boundingBoxPadding, maxY + boundingBoxPadding, maxX + boundingBoxPadding, minY - boundingBoxPadding);
 	}
-
+	
 	/*
      * DESCR: Determines the location for the camera.
      * PRE: A rectangle with an area containing the players.
@@ -106,7 +128,7 @@ public class cameraController : MonoBehaviour {
 		
 		return new Vector3(boundingBoxCenter.x, boundingBoxCenter.y, cam.transform.position.z);
 	}
-
+	
 	/*
      * DESCR: Determines a size for the camera.
      * PRE: A rectangle with an area containing the players.
@@ -125,5 +147,5 @@ public class cameraController : MonoBehaviour {
 		
 		return Mathf.Clamp(Mathf.Lerp(cam.orthographicSize, orthographicSize, Time.deltaTime * zoomSpeed), minimumOrthographicSize, Mathf.Infinity);
 	}
-
+	
 }
---
+++ RockPaperScissors/Assets/Scripts/voiceController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class voiceController : MonoBehaviour {
+	
+	private AudioSource voice;
+	
+	public AudioClip lightAttack;
+	public AudioClip heavyAttack;
+	public AudioClip specialAttack;
+	public AudioClip flinch;
+	public AudioClip launched;
+	public AudioClip death;
+	
+	// Use this for initialization
+	void Start () {
+		voice = this.audio;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void playLightAttack () {
+		voice.clip = lightAttack;
+		voice.Play ();
+	}
+	
+	void playHeavyAttack() {
+		voice.clip = heavyAttack;
+		voice.Play ();
+	}
+	
+	void playSpecialAttack() {
+		voice.clip = specialAttack;
+		voice.Play ();
+	}
+	
+	void playFlinch() {
+		voice.clip = flinch;
+		voice.Play ();
+	}
+	
+	void playLaunched() {
+		voice.clip = launched;
+		voice.Play ();
+	}
+	
+	void playDeath() {
+		voice.clip = death;
+		voice.Play ();
+	}
+	
+}
