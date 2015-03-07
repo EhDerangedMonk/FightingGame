d096a87793de229b6c6a33a11f58f04642ae0810
DaedalusLost <gibsonb@mail.uoguelph.ca>
2015-03-07 17:47:11 -0500

    Player indicators follow the characters
    Need to make the offset relative to the screen size

--- RockPaperScissors/Assets/Scripts/healthDisplay.cs
+++ RockPaperScissors/Assets/Scripts/healthDisplay.cs
@@ -8,8 +8,12 @@ using System.Collections;
 // TODO: make the health display above the players' heads
 
 public class healthDisplay : MonoBehaviour {
-    Player p1, p2;
-    GUIStyle labelFontSize;
+    private Player p1, p2;
+	private GUIStyle labelFontSize; // Formats the labels
+	private Vector3 P1ScreenPoint, P2ScreenPoint; // The locations of each player on the screen (in pixels)
+    public Texture P1Tag, P2Tag; // Floats above the players' heads
+    private Vector2 P1Offset, P2Offset; // How much the tags will be offset from the players' positions. Public so we can easily modify it.
+    
 
     // Use this for initialization
     void Start () {
@@ -22,20 +26,22 @@ public class healthDisplay : MonoBehaviour {
         labelFontSize = new GUIStyle();
         labelFontSize.fontSize = 30;
         labelFontSize.normal.textColor = Color.white;
+        
+        P1Offset = P2Offset = new Vector2(-17, -90);
     }
 
     // Update is called once per frame
     void Update () {
-
+    	// Get each player's screen position
+		P1ScreenPoint = Camera.main.WorldToScreenPoint(p1.transform.position);
+		P2ScreenPoint = Camera.main.WorldToScreenPoint(p2.transform.position);
     }
 
     void OnGUI() {
-		Vector3 P1ScreenPoint = Camera.main.WorldToScreenPoint(p1.transform.position);
-		Vector3 P2ScreenPoint = Camera.main.WorldToScreenPoint(p2.transform.position);
-		
-        GUI.Label (new Rect(10, 10, 300, 300), p1.playerHealth.getHealth().ToString(), labelFontSize);
-        GUI.Label (new Rect(P1ScreenPoint[0], Screen.height-P1ScreenPoint[1], 300, 300), P1ScreenPoint.ToString(), labelFontSize);
+		GUI.Label (new Rect(10, 10, 300, 300), p1.playerHealth.getHealth().ToString(), labelFontSize);
+        GUI.DrawTexture (new Rect(P1ScreenPoint[0]+P1Offset[0], Screen.height-P1ScreenPoint[1]+P1Offset[1], P1Tag.width/4, P1Tag.height/4), P1Tag);
         GUI.Label (new Rect(1200, 10, 300, 300), p2.playerHealth.getHealth().ToString(), labelFontSize);
-        GUI.Label (new Rect(P2ScreenPoint[0], Screen.height-P2ScreenPoint[1], 300, 300), P2ScreenPoint.ToString(), labelFontSize);
+        print ("P2 Health: " + p2.playerHealth.getHealth().ToString());
+        GUI.DrawTexture (new Rect(P2ScreenPoint[0]+P2Offset[0], Screen.height-P2ScreenPoint[1]+P2Offset[1], P2Tag.width/4, P2Tag.height/4), P2Tag);
     }
 }
