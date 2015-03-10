ea72d9db6b1dd33020089ba2fc30aeaf920b064d
DaedalusLost <gibsonb@mail.uoguelph.ca>
2015-03-10 18:43:15 -0400

    Tried modifying the indicator gap. Health moves.
    Somewhat failed. I'll be changing the indicators to objects that follow
    the players instead of GUI textures.

--- RockPaperScissors/Assets/Scripts/healthDisplay.cs
+++ RockPaperScissors/Assets/Scripts/healthDisplay.cs
@@ -10,10 +10,12 @@ using System.Collections;
 public class healthDisplay : MonoBehaviour {
     private Player p1, p2;
 	private GUIStyle labelFontSize; // Formats the labels
-	private Vector3 P1ScreenPoint, P2ScreenPoint; // The locations of each player on the screen (in pixels)
+	private Vector3 P1TopLeftPoint, P2TopLeftPoint; // The top left locations of each player on the screen (in pixels)
+	private Vector3 P1BottomPoint; // The bottom-centre of each character (in pixels). Used to calculate the relative height
+	private int CharacterScreenHeight; // The height of the player (in pixels) on the camera.
     public Texture P1Tag, P2Tag; // Floats above the players' heads
     private Vector2 P1Offset, P2Offset; // How much the tags will be offset from the players' positions. Public so we can easily modify it.
-    
+    private int n;
 
     // Use this for initialization
     void Start () {
@@ -28,20 +30,29 @@ public class healthDisplay : MonoBehaviour {
         labelFontSize.normal.textColor = Color.white;
         
         P1Offset = P2Offset = new Vector2(-17, -90);
+        n=0;
     }
 
     // Update is called once per frame
     void Update () {
-    	// Get each player's screen position
-		P1ScreenPoint = Camera.main.WorldToScreenPoint(p1.transform.position);
-		P2ScreenPoint = Camera.main.WorldToScreenPoint(p2.transform.position);
+    	// Get each player's top left screen position
+		P1TopLeftPoint = Camera.main.WorldToScreenPoint(p1.transform.position);
+		P2TopLeftPoint = Camera.main.WorldToScreenPoint(p2.transform.position);
+		
+		// Get each player's bottom-centre screen position
+		Vector3 extents = p1.renderer.bounds.extents;
+		CharacterScreenHeight = (int) Camera.main.WorldToScreenPoint(new Vector3(0f, extents.y*2, 0f)).y;
+		print ("Scale: " + ((float)CharacterScreenHeight/275f).ToString());
+		
+		// Adjust the offset based on the character's screen height
+		P1Offset = P2Offset = new Vector2((int) (-17f * ((float)CharacterScreenHeight/275f)), (int) ((float)(0 - P1Tag.height/4) - (40f * ((float)CharacterScreenHeight)/275f))); // divides the character's current height by their original height
+		
     }
 
     void OnGUI() {
-		GUI.Label (new Rect(10, 10, 300, 300), p1.playerHealth.getHealth().ToString(), labelFontSize);
-        GUI.DrawTexture (new Rect(P1ScreenPoint[0]+P1Offset[0], Screen.height-P1ScreenPoint[1]+P1Offset[1], P1Tag.width/4, P1Tag.height/4), P1Tag);
-        GUI.Label (new Rect(1200, 10, 300, 300), p2.playerHealth.getHealth().ToString(), labelFontSize);
-        print ("P2 Health: " + p2.playerHealth.getHealth().ToString());
-        GUI.DrawTexture (new Rect(P2ScreenPoint[0]+P2Offset[0], Screen.height-P2ScreenPoint[1]+P2Offset[1], P2Tag.width/4, P2Tag.height/4), P2Tag);
+		GUI.Label (new Rect(P1TopLeftPoint[0]+P1Offset[0], Screen.height-P1TopLeftPoint[1]+P1Offset[1]-40, 300, 300), p1.playerHealth.getHealth().ToString(), labelFontSize);
+        GUI.DrawTexture (new Rect(P1TopLeftPoint[0]+P1Offset[0], Screen.height-P1TopLeftPoint[1]+P1Offset[1], P1Tag.width/4, P1Tag.height/4), P1Tag);
+		GUI.Label (new Rect(P2TopLeftPoint[0]+P2Offset[0], Screen.height-P2TopLeftPoint[1]+P2Offset[1]-40, 300, 300), p2.playerHealth.getHealth().ToString(), labelFontSize);
+        GUI.DrawTexture (new Rect(P2TopLeftPoint[0]+P2Offset[0], Screen.height-P2TopLeftPoint[1]+P2Offset[1], P2Tag.width/4, P2Tag.height/4), P2Tag);
     }
 }
