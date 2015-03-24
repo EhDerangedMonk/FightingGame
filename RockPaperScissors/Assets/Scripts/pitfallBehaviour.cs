3940b0b6042dd06eca2eb786c39469a85e164430
Nigel Martinez <nigel-martinez@hotmail.com>
2015-03-24 15:06:54 -0400

    Smash Bros Explosions Now Added

--- RockPaperScissors/Assets/Scripts/pitfallBehaviour.cs
+++ RockPaperScissors/Assets/Scripts/pitfallBehaviour.cs
@@ -7,7 +7,11 @@ using UnityEngine;
 using System.Collections;
 
 public class pitfallBehaviour : MonoBehaviour {
-	Player victim;
+
+	public int orientation;
+
+	private Player victim;
+	private Vector3 spawnLocation;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +27,20 @@ public class pitfallBehaviour : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.tag == "Player") {
 			victim = (Player)other.gameObject.GetComponent(typeof(Player));
-			victim.playerHealth.damage(99999); //Instakill the player.
+			spawnLocation = victim.transform.position;
+
+			if(victim.playerHealth.isDead () == false) {
+				//Create an explosion in a fashion similar to Super Smash Bros.
+				GameObject explosion = Resources.Load("Screen Explosion") as GameObject;
+				if(orientation == 0) //Spawn from below the screen.
+					Instantiate (explosion, spawnLocation, transform.rotation);
+				else if(orientation == 1) //Spawn from the left of the screen.
+					Instantiate(explosion, spawnLocation, Quaternion.Euler (0,0,270));
+				else //Spawn from the right of the screen.
+					Instantiate(explosion, spawnLocation, Quaternion.Euler (0,0,90));
+			}
+
+			victim.playerHealth.kill ();
 		}
 	}
 }
