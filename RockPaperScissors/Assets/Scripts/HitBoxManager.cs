5412f2be1c15d559e86e51abd2183ed4cd60f1b1
Nigel Martinez <nigel-martinez@hotmail.com>
2015-03-27 15:18:39 -0400

    Falcon Punch?
    Right now, Zakir's hits don't work, but it might be due to how attacking
    is structured.
    
    Anyway, the trigger collider for his hitbox turns on and off at the
    correct frames.

---
+++ RockPaperScissors/Assets/Scripts/HitBoxManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBoxManager : MonoBehaviour {
+
+	private Collider2D hitBox;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.gameObject.GetComponent<Animator> ();
+		Collider2D[] colliders = this.gameObject.GetComponentsInChildren<Collider2D> ();
+
+		foreach (Collider2D collider in colliders) {
+			if(collider.isTrigger)
+				hitBox = collider;
+		}
+
+		disableHit ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.IsInTransition(0))
+		    disableHit();
+	}
+
+	void enableHit() {
+		hitBox.enabled = true;
+	}
+
+	void disableHit() {
+		hitBox.enabled = false;
+	}
+	
+}
