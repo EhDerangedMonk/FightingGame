2a0fd8ab4c917510fd7894c930085203cb30f1a2
DaedalusLost <gibsonb@mail.uoguelph.ca>
2015-03-29 17:31:40 -0400

    Health displays showing for players 3 & 4
    I was only able to test P3, but P4 should work in theory.

--- RockPaperScissors/Assets/Scripts/healthDisplay.cs
+++ RockPaperScissors/Assets/Scripts/healthDisplay.cs
@@ -8,15 +8,16 @@ using System.Collections;
 public class healthDisplay : MonoBehaviour {
 
 	private InitializeStorage gameSettings;
-    private Player p1, p2;
+    private Player p1, p2, p3, p4;
 	private GUIStyle labelFontSize; // Formats the labels
-	private Vector3 P1TagPoint, P2TagPoint; // The top left locations of each player on the screen (in pixels)
-	private Vector3 P1HealthPoint, P2HealthPoint; // Where to draw the health indicators.
-    public Texture P1Tag, P2Tag; // Floats above the players' heads
+	private Vector3 P1TagPoint, P2TagPoint, P3TagPoint, P4TagPoint; // The top left locations of each player on the screen (in pixels)
+	private Vector3 P1HealthPoint, P2HealthPoint, P3HealthPoint, P4HealthPoint; // Where to draw the health indicators.
+    public Texture P1Tag, P2Tag, P3Tag, P4Tag; // Floats above the players' heads
     public Texture Health_100to81, Health_80to61, Health_60to41, Health_40to21, Health_20to1; // Indicate the players' health levels
     private int n;
 	private float xTagOffset, yTagOffset; // How much the tags will be offset from the players' positions. 
 	public float xHealthOffset, yHealthOffset; // How much the health indicators will be offset from the players' positions.
+	private bool P3Playing, P4Playing; // Determine if these players are playing
 	
     // Use this for initialization
     void Start () {
@@ -32,6 +33,17 @@ public class healthDisplay : MonoBehaviour {
         yTagOffset = 3.3f; // These are just where it looks good, they can be modified
         
         n=0;
+        
+        // Determine if P3 and P4 are playing
+		InitializeStorage settings = (InitializeStorage) GameObject.Find("VariableStorage").GetComponent(typeof(InitializeStorage));
+		if ((int)settings.P3Controller == 0)
+			P3Playing = false;
+		else
+			P3Playing = true;
+		if ((int)settings.P4Controller == 0)
+			P4Playing = false;
+		else
+			P4Playing = true;
     }
 
     // Update is called once per frame
@@ -46,13 +58,30 @@ public class healthDisplay : MonoBehaviour {
 				if(subject.layout == (int)gameSettings.P1Controller)
 					p1 = subject;
 				else if(subject.layout == (int)gameSettings.P2Controller)
-					p2 =subject;
+					p2 = subject;
+				else if (subject.layout == (int)gameSettings.P3Controller)
+					p3 = subject;
+				else if (subject.layout == (int)gameSettings.P4Controller)
+					p4 = subject;
 			}
 		}
 
     	// Get each player's top left screen position
     	Vector3 P1Location = p1.transform.position;
     	Vector3 P2Location = p2.transform.position;
+    	Vector3 P3Location, P4Location = new Vector3(0f, 0f, 0f);
+    	if (P3Playing)
+    	{
+			P3Location = p3.transform.position;
+			P3TagPoint = Camera.main.WorldToScreenPoint(new Vector3(P3Location.x+xTagOffset, P3Location.y+yTagOffset, P3Location.z));
+			P3HealthPoint = Camera.main.WorldToScreenPoint(new Vector3(P3Location.x+xHealthOffset, P3Location.y+yHealthOffset, P3Location.z));
+    	}
+    	if (P4Playing)
+    	{
+			P4Location = p4.transform.position;
+			P4TagPoint = Camera.main.WorldToScreenPoint(new Vector3(P4Location.x+xTagOffset, P4Location.y+yTagOffset, P4Location.z));
+			P4HealthPoint = Camera.main.WorldToScreenPoint(new Vector3(P4Location.x+xHealthOffset, P4Location.y+yHealthOffset, P4Location.z));
+		}
     	
 		P1TagPoint = Camera.main.WorldToScreenPoint(new Vector3(P1Location.x+xTagOffset, P1Location.y+yTagOffset, P1Location.z));
 		P2TagPoint = Camera.main.WorldToScreenPoint(new Vector3(P2Location.x+xTagOffset, P2Location.y+yTagOffset, P2Location.z));
@@ -89,5 +118,39 @@ public class healthDisplay : MonoBehaviour {
 			GUI.DrawTexture (new Rect(P2HealthPoint.x, Screen.height-P2HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_40to21);
 		else if (1 <= p2.playerHealth.getHealth() && p2.playerHealth.getHealth() <=200)
 			GUI.DrawTexture (new Rect(P2HealthPoint.x, Screen.height-P2HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_20to1);
+		
+		if (P3Playing)
+		{
+			GUI.Label (new Rect(P3TagPoint.x, Screen.height-P3TagPoint.y-40, 300, 300), p3.playerHealth.getHealth().ToString(), labelFontSize);
+			GUI.DrawTexture (new Rect(P3TagPoint.x, Screen.height-P3TagPoint.y, P3Tag.width/4, P3Tag.height/4), P3Tag);
+			// Determine which health indicator texture to draw based on the player's health
+			if (801 <= p3.playerHealth.getHealth() && p3.playerHealth.getHealth() <= 1000)
+				GUI.DrawTexture (new Rect(P3HealthPoint.x, Screen.height-P3HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_100to81);
+			else if (601 <= p3.playerHealth.getHealth() && p3.playerHealth.getHealth() <=800)
+				GUI.DrawTexture (new Rect(P3HealthPoint.x, Screen.height-P3HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_80to61);
+			else if (401 <= p3.playerHealth.getHealth() && p3.playerHealth.getHealth() <=600)
+				GUI.DrawTexture (new Rect(P3HealthPoint.x, Screen.height-P3HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_60to41);
+			else if (201 <= p3.playerHealth.getHealth() && p3.playerHealth.getHealth() <=400)
+				GUI.DrawTexture (new Rect(P3HealthPoint.x, Screen.height-P3HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_40to21);
+			else if (1 <= p3.playerHealth.getHealth() && p3.playerHealth.getHealth() <=200)
+				GUI.DrawTexture (new Rect(P3HealthPoint.x, Screen.height-P3HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_20to1);
+		}
+		
+		if (P4Playing)
+		{
+			GUI.Label (new Rect(P4TagPoint.x, Screen.height-P4TagPoint.y-40, 300, 300), p4.playerHealth.getHealth().ToString(), labelFontSize);
+			GUI.DrawTexture (new Rect(P4TagPoint.x, Screen.height-P4TagPoint.y, P4Tag.width/4, P4Tag.height/4), P4Tag);
+			// Determine which health indicator texture to draw based on the player's health
+			if (801 <= p4.playerHealth.getHealth() && p4.playerHealth.getHealth() <= 1000)
+				GUI.DrawTexture (new Rect(P4HealthPoint.x, Screen.height-P4HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_100to81);
+			else if (601 <= p4.playerHealth.getHealth() && p4.playerHealth.getHealth() <=800)
+				GUI.DrawTexture (new Rect(P4HealthPoint.x, Screen.height-P4HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_80to61);
+			else if (401 <= p4.playerHealth.getHealth() && p4.playerHealth.getHealth() <=600)
+				GUI.DrawTexture (new Rect(P4HealthPoint.x, Screen.height-P4HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_60to41);
+			else if (201 <= p4.playerHealth.getHealth() && p4.playerHealth.getHealth() <=400)
+				GUI.DrawTexture (new Rect(P4HealthPoint.x, Screen.height-P4HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_40to21);
+			else if (1 <= p4.playerHealth.getHealth() && p4.playerHealth.getHealth() <=200)
+				GUI.DrawTexture (new Rect(P4HealthPoint.x, Screen.height-P4HealthPoint.y, Health_100to81.width/3, Health_100to81.height/3), Health_20to1);
+		}
 	}
 }
